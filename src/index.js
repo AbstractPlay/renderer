a05df6012c00b4d9f22133283ec2e7d8d16ed83e
Aaron Dalton <aaron@daltons.ca>
2019-01-19 13:42:53 -0700

    Updated some versions, but I'm going to move to Typescript. Pushing these changes before making that switch.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Basic 9 palette: ['#e41a1c', '#377eb8', '#4daf4a', '#984ea3', '#ff7f00', '#ffff33', '#a65628', '#f781bf', '#999999']
+// Color-blind friendly 4 palette: ['#a6611a','#018571','#dfc27d','#80cdc1']
+
+exports.renderAbstractPlay = function(divid, renderrep) {
+    console.log("In the render function.");
+    //Validate json
+    //Render it
+}
