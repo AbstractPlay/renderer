68f1afc9848420ac22efae9dd6865029cf746789
Aaron Dalton <aaron@daltons.ca>
2019-02-02 11:20:13 -0700

    Removed the debug test because my local test render environment is working

--- test/index.test.ts
+++ test/index.test.ts
@@ -24,30 +24,3 @@ test("schema validator", () => {
     const valid = ajv.validate(schema, data);
     expect(valid).toBe(true);
 });
-
-test("debugging outputter", () => {
-    const data = {
-        board: {style: "squares-checkered"},
-        legend: {
-            B: {name: "piece", colour: 2},
-            G: {name: "piece", colour: 3},
-            R: {name: "piece", colour: 1},
-            Y: {name: "piece", colour: 4},
-        },
-        pieces: "YYRR\nY--R\nB--G\nBBGG",
-    };
-    const window = require("svgdom");
-    // v3
-    // const {SVG, registerWindow} = require("@svgdotjs/svg.js");
-    // registerWindow(window, window.document);
-    // const canvas = SVG(document.documentElement);
-
-    // v2
-    const SVG = require("svg.js")(window);
-    const document = window.document;
-    const canvas = SVG(document.documentElement);
-
-    render(data, {divid: "", target: canvas});
-    console.log(canvas.svg());
-    // const canvas = SVG(document.documentElement);
-});
