53d662ba45b2f2c6c6f99483a8421ee3f9447024
Aaron Dalton <aaron@daltons.ca>
2024-07-03 12:19:17 -0600

    Add blocking to all vertex boards

---
+++ src/graphs/index.ts
@@ -0,0 +1,8 @@
+import { SquareGraph } from "./square";
+import { SquareOrthGraph } from "./square-orth";
+import { SquareDiagGraph } from "./square-diag";
+import { SquareFanoronaGraph } from "./square-fanorona";
+import { Graph } from "./Graph";
+
+export { Graph, SquareGraph, SquareOrthGraph, SquareDiagGraph, SquareFanoronaGraph };
+
