180afd2784a09f1e3c51ed3a1341608403668580
Aaron Dalton <aaron@daltons.ca>
2024-08-30 13:53:51 -0600

    Add traditional chess glyphs

--- src/sheets/chess.ts
+++ src/sheets/chess.ts
@@ -42,6 +42,25 @@ sheet.glyphs.set("chess-bishop-outline-montreal", (canvas: SVGContainer) => {
     return group;
 });
 
+sheet.glyphs.set("chess-bishop-outline-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#fff", opacity: 1, rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round", miterlimit: 4})
+        .translate(0, 0.6);
+    const bGroup = group.group()
+        .stroke({linecap: "butt"});
+    bGroup.path("M 9,36 C 12.39,35.03 19.11,36.43 22.5,34 C 25.89,36.43 32.61,35.03 36,36 C 36,36 37.65,36.54 39,38 C 38.32,38.97 37.35,38.99 36,38.5 C 32.61,37.53 25.89,38.96 22.5,37.5 C 19.11,38.96 12.39,37.53 9,38.5 C 7.65,38.99 6.68,38.97 6,38 C 7.35,36.54 9,36 9,36 z");
+    bGroup.path("M 15,32 C 17.5,34.5 27.5,34.5 30,32 C 30.5,30.5 30,30 30,30 C 30,27.5 27.5,26 27.5,26 C 33,24.5 33.5,14.5 22.5,10.5 C 11.5,14.5 12,24.5 17.5,26 C 17.5,26 15,27.5 15,30 C 15,30 14.5,30.5 15,32 z");
+    bGroup.path("M 25 8 A 2.5 2.5 0 1 1  20,8 A 2.5 2.5 0 1 1  25 8 z");
+    group.path("M 17.5,26 L 27.5,26 M 15,30 L 30,30 M 22.5,15.5 L 22.5,20.5 M 20,18 L 25,18")
+        .attr("data-context-stroke", true)
+        .stroke({color: "#000", linejoin: "miter"});
+    symbol.viewbox(5.092, 5.35, 34.816, 34.816);
+    return symbol;
+});
+
 sheet.glyphs.set("chess-bishop-solid-line", (canvas: SVGContainer) => {
     const group = canvas.symbol();
     group.path("M1159 2252h1777v-1982h-1777v1982zM1374 3141l673 673l674 -674l-673 -673z")
@@ -72,6 +91,25 @@ sheet.glyphs.set("chess-bishop-solid-montreal", (canvas: SVGContainer) => {
         .fill("#000");
     group.viewbox(60, 60, 880, 880);
     return group;
+});
+
+sheet.glyphs.set("chess-bishop-solid-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#000", opacity: 1, rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round", miterlimit: 4})
+        .translate(0, 0.6);
+    const bGroup = group.group()
+        .stroke({linecap: "butt"});
+    bGroup.path("M 9,36 C 12.39,35.03 19.11,36.43 22.5,34 C 25.89,36.43 32.61,35.03 36,36 C 36,36 37.65,36.54 39,38 C 38.32,38.97 37.35,38.99 36,38.5 C 32.61,37.53 25.89,38.96 22.5,37.5 C 19.11,38.96 12.39,37.53 9,38.5 C 7.65,38.99 6.68,38.97 6,38 C 7.35,36.54 9,36 9,36 z");
+    bGroup.path("M 15,32 C 17.5,34.5 27.5,34.5 30,32 C 30.5,30.5 30,30 30,30 C 30,27.5 27.5,26 27.5,26 C 33,24.5 33.5,14.5 22.5,10.5 C 11.5,14.5 12,24.5 17.5,26 C 17.5,26 15,27.5 15,30 C 15,30 14.5,30.5 15,32 z");
+    bGroup.path("M 25 8 A 2.5 2.5 0 1 1  20,8 A 2.5 2.5 0 1 1  25 8 z");
+    group.path("M 17.5,26 L 27.5,26 M 15,30 L 30,30 M 22.5,15.5 L 22.5,20.5 M 20,18 L 25,18")
+        .attr("data-context-stroke", true)
+        .stroke({color: "#fff", linejoin: "miter"});
+    symbol.viewbox(5.092, 5.35, 34.816, 34.816);
+    return symbol;
 });
 
 sheet.glyphs.set("chess-ex-solid-line", (canvas: SVGContainer) => {
@@ -123,6 +161,24 @@ sheet.glyphs.set("chess-king-outline-montreal", (canvas: SVGContainer) => {
     return group;
 });
 
+sheet.glyphs.set("chess-king-outline-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "none", rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round"})
+    group.path("M22.5 11.63V6M20 8h5")
+        .stroke({color: "#000", linejoin: "miter"});
+    group.path("M22.5 25s4.5-7.5 3-10.5c0 0-1-2.5-3-2.5s-3 2.5-3 2.5c-1.5 3 3 10.5 3 10.5")
+        // .fill("#fff")
+        .stroke({linejoin: "miter", linecap: "butt"})
+    group.path("M12.5 37c5.5 3.5 14.5 3.5 20 0v-7s9-4.5 6-10.5c-4-6.5-13.5-3.5-16 4V27v-3.5c-2.5-7.5-12-10.5-16-4-3 6 6 10.5 6 10.5v7")
+        // .fill("#fff")
+    group.path("M12.5 30c5.5-3 14.5-3 20 0m-20 3.5c5.5-3 14.5-3 20 0m-20 3.5c5.5-3 14.5-3 20 0")
+    symbol.viewbox(4.922, 5.25, 35.157, 35.157);
+    return symbol;
+});
+
 sheet.glyphs.set("chess-king-solid-line", (canvas: SVGContainer) => {
     const group = canvas.symbol();
     group.path("M2048 3348h-612v271h477v207h270v-207h476v-271h-611zM1159 1298h1777v-1028h-1777v1028zM2392 2096h369l356 518l-360 2zM1703 2096l-361 518h-363l367 -518h357zM1900 2616v-520h297v520h-297zM2936 1570h-1777l-966 1499h3710z")
@@ -155,6 +211,29 @@ sheet.glyphs.set("chess-king-solid-montreal", (canvas: SVGContainer) => {
     return group;
 });
 
+sheet.glyphs.set("chess-king-solid-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#000", opacity: 1, rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round", miterlimit: 4})
+    group.path("M 22.5,11.63 L 22.5,6")
+        .stroke({linejoin: "miter"});
+    group.path("M 22.5,25 C 22.5,25 27,17.5 25.5,14.5 C 25.5,14.5 24.5,12 22.5,12 C 20.5,12 19.5,14.5 19.5,14.5 C 18,17.5 22.5,25 22.5,25")
+        .stroke({linecap: "butt", linejoin: "miter"});
+    group.path("M 12.5,37 C 18,40.5 27,40.5 32.5,37 L 32.5,30 C 32.5,30 41.5,25.5 38.5,19.5 C 34.5,13 25,16 22.5,23.5 L 22.5,27 L 22.5,23.5 C 20,16 10.5,13 6.5,19.5 C 3.5,25.5 12.5,30 12.5,30 L 12.5,37");
+    group.path("M 20,8 L 25,8")
+        .stroke({linejoin: "miter"});
+    group.path("M 32,29.5 C 32,29.5 40.5,25.5 38.03,19.85 C 34.15,14 25,18 22.5,24.5 L 22.5,26.6 L 22.5,24.5 C 20,18 10.85,14 6.97,19.85 C 4.5,25.5 13,29.5 13,29.5")
+        .stroke("#fff")
+        .attr("data-context-stroke", true);
+    group.path("M 12.5,30 C 18,27 27,27 32.5,30 M 12.5,33.5 C 18,30.5 27,30.5 32.5,33.5 M 12.5,37 C 18,34 27,34 32.5,37")
+        .stroke("#fff")
+        .attr("data-context-stroke", true);
+    symbol.viewbox(4.922, 5.250, 35.157, 35.157);
+    return symbol;
+});
+
 sheet.glyphs.set("chess-knight-outline-line", (canvas: SVGContainer) => {
     const group = canvas.symbol();
     group.path("M2014 2867q185 0 185 -185t-185 -185q-184 0 -184 185t184 185zM3031 1155h-271v734l-394 395l268 120l398 -399zM3562 549v-279h-2384l-2 278l549 1174l-767 -280l-431 924l1488 1042l-214 216l196 196l1565 -1576v-1695zM2095 1860l-612 -1312h1809l1 1587l-1075 1075 l-1341 -938l223 -479l1059 386l95 -260z")
@@ -182,6 +261,23 @@ sheet.glyphs.set("chess-knight-outline-montreal", (canvas: SVGContainer) => {
         .fill("#000");
     group.viewbox(40, 40, 920, 920);
     return group;
+});
+
+sheet.glyphs.set("chess-knight-outline-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#fff", opacity: 1, rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round", miterlimit: 4})
+        .translate(0, 0.3);
+    group.path("M 22,10 C 32.5,11 38.5,18 38,39 L 15,39 C 15,30 25,32.5 23,18");
+    group.path("M 24,18 C 24.38,20.91 18.45,25.37 16,27 C 13,29 13.18,31.34 11,31 C 9.958,30.06 12.41,27.96 11,28 C 10,28 11.19,29.23 10,30 C 9,30 5.997,31 6,26 C 6,24 12,14 12,14 C 12,14 13.89,12.1 14,10.5 C 13.27,9.506 13.5,8.5 13.5,7.5 C 14.5,6.5 16.5,10 16.5,10 L 18.5,10 C 18.5,10 19.28,8.008 21,7 C 22,7 22,10 22,10");
+    group.path("M 9.5 25.5 A 0.5 0.5 0 1 1 8.5,25.5 A 0.5 0.5 0 1 1 9.5 25.5 z")
+        .fill("#000");
+    group.path("M 15 15.5 A 0.5 1.5 0 1 1  14,15.5 A 0.5 1.5 0 1 1  15 15.5 z")
+        .matrix(0.866,0.5,-0.5,0.866,9.693,-5.173);
+    symbol.viewbox(5.25, 6.5355, 33.529, 33.529);
+    return symbol;
 });
 
 sheet.glyphs.set("chess-knight-solid-line", (canvas: SVGContainer) => {
@@ -216,6 +312,35 @@ sheet.glyphs.set("chess-knight-solid-montreal", (canvas: SVGContainer) => {
     return group;
 });
 
+sheet.glyphs.set("chess-knight-solid-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#000", opacity: 1, rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round", miterlimit: 4})
+        .translate(0, 0.3);
+    group.path("M 22,10 C 32.5,11 38.5,18 38,39 L 15,39 C 15,30 25,32.5 23,18");
+    group.path("M 24,18 C 24.38,20.91 18.45,25.37 16,27 C 13,29 13.18,31.34 11,31 C 9.958,30.06 12.41,27.96 11,28 C 10,28 11.19,29.23 10,30 C 9,30 5.997,31 6,26 C 6,24 12,14 12,14 C 12,14 13.89,12.1 14,10.5 C 13.27,9.506 13.5,8.5 13.5,7.5 C 14.5,6.5 16.5,10 16.5,10 L 18.5,10 C 18.5,10 19.28,8.008 21,7 C 22,7 22,10 22,10");
+    const bGroup = group.group()
+        .attr("data-context-stroke", true)
+        .stroke({color: "#fff"});
+    bGroup.path("M 9.5 25.5 A 0.5 0.5 0 1 1 8.5,25.5 A 0.5 0.5 0 1 1 9.5 25.5 z")
+        .attr("data-context-background", true)
+        .fill("#fff");
+    bGroup.path("M 15 15.5 A 0.5 1.5 0 1 1  14,15.5 A 0.5 1.5 0 1 1  15 15.5 z")
+        .matrix(0.866,0.5,-0.5,0.866,9.693,-5.173)
+        .attr("data-context-stroke", true)
+        .attr("data-context-background", true)
+        .fill("#fff")
+        .stroke({color: "#fff", linejoin: "miter"});
+    bGroup.path("M 24.55,10.4 L 24.1,11.85 L 24.6,12 C 27.75,13 30.25,14.49 32.5,18.75 C 34.75,23.01 35.75,29.06 35.25,39 L 35.2,39.5 L 37.45,39.5 L 37.5,39 C 38,28.94 36.62,22.15 34.25,17.66 C 31.88,13.17 28.46,11.02 25.06,10.5 L 24.55,10.4 z")
+        .attr("data-context-background", true)
+        .stroke("none")
+        .fill("#fff");
+    symbol.viewbox(5.25, 6.5355, 33.529, 33.529);
+    return symbol;
+});
+
 sheet.glyphs.set("chess-pawn-outline-line", (canvas: SVGContainer) => {
     const group = canvas.symbol();
     group.path("M1159 1298h1777v-1028h-1777v1028zM1436 1027v-479h1223v479h-1223zM1842 2255v-414h412v414h-412zM1572 2526h952v-956h-952v956z")
@@ -244,6 +369,16 @@ sheet.glyphs.set("chess-pawn-outline-montreal", (canvas: SVGContainer) => {
         .fill("#000");
     group.viewbox(280, 280, 440, 440);
     return group;
+});
+
+sheet.glyphs.set("chess-pawn-outline-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    symbol.path("m 22.5,9 c -2.21,0 -4,1.79 -4,4 0,0.89 0.29,1.71 0.78,2.38 C 17.33,16.5 16,18.59 16,21 c 0,2.03 0.94,3.84 2.41,5.03 C 15.41,27.09 11,31.58 11,39.5 H 34 C 34,31.58 29.59,27.09 26.59,26.03 28.06,24.84 29,23.03 29,21 29,18.59 27.67,16.5 25.72,15.38 26.21,14.71 26.5,13.89 26.5,13 c 0,-2.21 -1.79,-4 -4,-4 z")
+        .attr("data-playerfill", true)
+        .fill({color: "#fff", rule: "nonzero"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "miter", miterlimit: 4});
+    symbol.viewbox(6.5, 8.25, 32, 32);
+    return symbol;
 });
 
 sheet.glyphs.set("chess-pawn-solid-line", (canvas: SVGContainer) => {
@@ -277,6 +412,16 @@ sheet.glyphs.set("chess-pawn-solid-montreal", (canvas: SVGContainer) => {
     return group;
 });
 
+sheet.glyphs.set("chess-pawn-solid-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    symbol.path("m 22.5,9 c -2.21,0 -4,1.79 -4,4 0,0.89 0.29,1.71 0.78,2.38 C 17.33,16.5 16,18.59 16,21 c 0,2.03 0.94,3.84 2.41,5.03 C 15.41,27.09 11,31.58 11,39.5 H 34 C 34,31.58 29.59,27.09 26.59,26.03 28.06,24.84 29,23.03 29,21 29,18.59 27.67,16.5 25.72,15.38 26.21,14.71 26.5,13.89 26.5,13 c 0,-2.21 -1.79,-4 -4,-4 z")
+        .attr("data-playerfill", true)
+        .fill({color: "#000", rule: "nonzero"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "miter", miterlimit: 4});
+    symbol.viewbox(6.5, 8.25, 32, 32);
+    return symbol;
+});
+
 sheet.glyphs.set("chess-queen-outline-line", (canvas: SVGContainer) => {
     const group = canvas.symbol();
     group.path("M2048 2120h-285l285 488l281 -488h-281zM3282 2622l-802 -218l-432 749l-431 -747l-802 214l508 -776h1450zM3905 3068l-969 -1498h-1777l-965 1498l1291 -349l563 975l564 -973zM1436 1027v-479h1223v479h-1223zM1159 1298h1777v-1028h-1777v1028z")
@@ -304,6 +449,25 @@ sheet.glyphs.set("chess-queen-outline-montreal", (canvas: SVGContainer) => {
         .fill("#000");
     group.viewbox(40, 40, 920, 920);
     return group;
+});
+
+sheet.glyphs.set("chess-queen-outline-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill("#fff")
+        .stroke({color: "#000", width: 1.5, linejoin: "round"})
+    group.path("M 9,26 C 17.5,24.5 30,24.5 36,26 L 38.5,13.5 L 31,25 L 30.7,10.9 L 25.5,24.5 L 22.5,10 L 19.5,24.5 L 14.3,10.9 L 14,25 L 6.5,13.5 L 9,26 z");
+    group.path("M 9,26 C 9,28 10.5,28 11.5,30 C 12.5,31.5 12.5,31 12,33.5 C 10.5,34.5 11,36 11,36 C 9.5,37.5 11,38.5 11,38.5 C 17.5,39.5 27.5,39.5 34,38.5 C 34,38.5 35.5,37.5 34,36 C 34,36 34.5,34.5 33,33.5 C 32.5,31 32.5,31.5 33.5,30 C 34.5,28 36,28 36,26 C 27.5,24.5 17.5,24.5 9,26 z");
+    group.path("M 11.5,30 C 15,29 30,29 33.5,30");
+    group.path("M 12,33.5 C 18,32.5 27,32.5 33,33.5");
+    group.circle(4).center(6, 12);
+    group.circle(4).center(14, 9);
+    group.circle(4).center(22.5, 8);
+    group.circle(4).center(31, 9);
+    group.circle(4).center(39, 12);
+    symbol.viewbox(3.25, 3.9715, 38.5, 38.5);
+    return symbol;
 });
 
 sheet.glyphs.set("chess-queen-solid-line", (canvas: SVGContainer) => {
@@ -338,6 +502,37 @@ sheet.glyphs.set("chess-queen-solid-montreal", (canvas: SVGContainer) => {
     return group;
 });
 
+sheet.glyphs.set("chess-queen-solid-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#000", opacity: 1})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round"})
+    group.path("M 9,26 C 17.5,24.5 30,24.5 36,26 L 38.5,13.5 L 31,25 L 30.7,10.9 L 25.5,24.5 L 22.5,10 L 19.5,24.5 L 14.3,10.9 L 14,25 L 6.5,13.5 L 9,26 z")
+        .stroke({linecap: "butt"});
+    group.path("m 9,26 c 0,2 1.5,2 2.5,4 1,1.5 1,1 0.5,3.5 -1.5,1 -1,2.5 -1,2.5 -1.5,1.5 0,2.5 0,2.5 6.5,1 16.5,1 23,0 0,0 1.5,-1 0,-2.5 0,0 0.5,-1.5 -1,-2.5 -0.5,-2.5 -0.5,-2 0.5,-3.5 1,-2 2.5,-2 2.5,-4 -8.5,-1.5 -18.5,-1.5 -27,0 z");
+    group.path("M 11.5,30 C 15,29 30,29 33.5,30");
+    group.path("m 12,33.5 c 6,-1 15,-1 21,0");
+    group.circle(4).center(6, 12);
+    group.circle(4).center(14, 9);
+    group.circle(4).center(22.5, 8);
+    group.circle(4).center(31, 9);
+    group.circle(4).center(39, 12);
+    group.path("M 11,38.5 A 35,35 1 0 0 34,38.5")
+        .fill("none")
+        .stroke({linecap: "butt"})
+    const lgroup = group.group()
+        .fill("none")
+        .stroke({color: "#fff"})
+        .attr("data-context-stroke", true);
+    lgroup.path("M 11,29 A 35,35 1 0 1 34,29");
+    lgroup.path("M 12.5,31.5 L 32.5,31.5");
+    lgroup.path("M 11.5,34.5 A 35,35 1 0 0 33.5,34.5");
+    lgroup.path("M 10.5,37.5 A 35,35 1 0 0 34.5,37.5");
+    symbol.viewbox(3.25, 3.9715, 38.5, 38.5);
+    return symbol;
+});
+
 sheet.glyphs.set("chess-rook-outline-line", (canvas: SVGContainer) => {
     const group = canvas.symbol();
     group.path("M1436 1027v-479h1223v479h-1223zM1159 1298h1777v-1028h-1777v1028zM2048 2255h-135v543h-477v-543h-277v543h-476v-957h2729v957h-476v-543h-277v543h-476v-543h-135zM406 3076h3277v-1506h-3277v1506z")
@@ -365,6 +560,24 @@ sheet.glyphs.set("chess-rook-outline-montreal", (canvas: SVGContainer) => {
         .fill("#000");
     group.viewbox(142, 142, 716, 716);
     return group;
+});
+
+sheet.glyphs.set("chess-rook-outline-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#fff", opacity: 1, rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round", miterlimit: 4})
+        .translate(0, 0.3);
+    group.path("M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 z").stroke({linecap: "butt"});
+    group.path("M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 z").stroke({linecap: "butt"});
+    group.path("M 11,14 L 11,9 L 15,9 L 15,11 L 20,11 L 20,9 L 25,9 L 25,11 L 30,11 L 30,9 L 34,9 L 34,14").stroke({linecap: "butt"});
+    group.path("M 34,14 L 31,17 L 14,17 L 11,14");
+    group.path("M 31,17 L 31,29.5 L 14,29.5 L 14,17").stroke({linecap: "butt", linejoin: "miter"});
+    group.path("M 31,29.5 L 32.5,32 L 12.5,32 L 14,29.5");
+    group.path("M 11,14 L 34,14").stroke({linejoin: "miter"});
+    symbol.viewbox(6.75, 8.55, 31.5, 31.5);
+    return symbol;
 });
 
 sheet.glyphs.set("chess-rook-solid-line", (canvas: SVGContainer) => {
@@ -397,6 +610,34 @@ sheet.glyphs.set("chess-rook-solid-montreal", (canvas: SVGContainer) => {
         .fill("#000");
     group.viewbox(142, 142, 716, 716);
     return group;
+});
+
+sheet.glyphs.set("chess-rook-solid-traditional", (canvas: SVGContainer) => {
+    const symbol = canvas.symbol();
+    const group = symbol.group()
+        .attr("data-playerfill", true)
+        .fill({color: "#000", opacity: 1, rule: "evenodd"})
+        .stroke({color: "#000", width: 1.5, linecap: "round", linejoin: "round", miterlimit: 4})
+        .translate(0, 0.3);
+    const bgroup = group.group()
+        .stroke({linecap: "butt"});
+    bgroup.path("M 9,39 L 36,39 L 36,36 L 9,36 L 9,39 z");
+    bgroup.path("M 12.5,32 L 14,29.5 L 31,29.5 L 32.5,32 L 12.5,32 z");
+    bgroup.path("M 12,36 L 12,32 L 33,32 L 33,36 L 12,36 z");
+    bgroup.path("M 14,29.5 L 14,16.5 L 31,16.5 L 31,29.5 L 14,29.5 z").stroke({linejoin: "miter"});
+    bgroup.path("M 14,16.5 L 11,14 L 34,14 L 31,16.5 L 14,16.5 z");
+    bgroup.path("M 11,14 L 11,9 L 15,9 L 15,11 L 20,11 L 20,9 L 25,9 L 25,11 L 30,11 L 30,9 L 34,9 L 34,14 L 11,14 z");
+    const lgroup = group.group()
+        .attr("data-context-stroke", true)
+        .fill("none")
+        .stroke({color: "#fff", width: 1, linejoin: "miter"});
+    lgroup.path("M 12,35.5 L 33,35.5 L 33,35.5");
+    lgroup.path("M 13,31.5 L 32,31.5");
+    lgroup.path("M 14,29.5 L 31,29.5");
+    lgroup.path("M 14,16.5 L 31,16.5");
+    lgroup.path("M 11,14 L 34,14");
+    symbol.viewbox(6.75, 8.55, 31.5, 31.5);
+    return symbol;
 });
 
 export { sheet as ChessSheet };
