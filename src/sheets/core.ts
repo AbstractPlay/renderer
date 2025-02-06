b3ee42aa24cbf4cfcc1a0c67aba635ae5e7c7bb2
Aaron Dalton <aaron@daltons.ca>
2025-02-06 16:40:01 -0700

    Add Chevron glyph

--- src/sheets/core.ts
+++ src/sheets/core.ts
@@ -110,6 +110,17 @@ sheet.glyphs.set("castle-solid", (canvas: SVGContainer) => {
     return sym;
 });
 
+sheet.glyphs.set("chevron", (canvas: SVGContainer) => {
+    const sym = canvas.symbol();
+    // sym.rect(40, 40).move(513.5, 1200).fill("red");
+    sym.path("M544.345,1213.39 L534.615,1202.6 C534.167,1202.15 533.57,1201.95 532.984,1201.99 C532.398,1201.95 531.802,1202.15 531.354,1202.6 L521.624,1213.39 C520.797,1214.22 520.797,1215.57 521.624,1216.4 C522.452,1217.23 523.793,1217.23 524.621,1216.4 L532.984,1207.13 L541.349,1216.4 C542.176,1217.23 543.518,1217.23 544.345,1216.4 C545.172,1215.57 545.172,1214.22 544.345,1213.39")
+        .stroke("none")
+        .fill({color: "#000", rule: "evenodd"})
+        .attr("data-playerfill", true);
+    sym.viewbox(513.5, 1200, 40, 40);
+    return sym;
+});
+
 sheet.glyphs.set("circle", (canvas: SVGContainer) => {
     const group = canvas.symbol();
     const border = 15;
