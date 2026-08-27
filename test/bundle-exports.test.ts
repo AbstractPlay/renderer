68672f329d7ff36980c7b8745772e6b850339937
Aaron Dalton <aaron@daltons.ca>
2026-08-27 08:01:55 -0600

    Harden ESM/CJS tests

--- test/bundle-exports.test.ts
+++ test/bundle-exports.test.ts
@@ -1,8 +1,10 @@
 import { expect } from "chai";
 import fs from "fs";
 import path from "path";
+import { createRequire } from "module";
 
 const buildIndex = path.join(process.cwd(), "build", "index.js");
+const require = createRequire(import.meta.url);
 
 describe("bundled build exports", function () {
     before(function () {
@@ -20,5 +22,12 @@ describe("bundled build exports", function () {
         expect(addPrefix).to.be.a("function");
         expect(sheets).to.be.instanceOf(Map);
         expect(sheets.size).to.be.greaterThan(0);
+    });
+
+    it("should load via CommonJS require (Lambda/thumbnails path)", () => {
+        const mod = require("../build/index.js");
+        const api = mod.default ?? mod;
+        expect(api.addPrefix).to.be.a("function");
+        expect(api.render).to.be.a("function");
     });
 });
